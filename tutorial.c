e2c3e50602814207fd04072975709ffd2241679f
Marc <marclaffan@gmail.com>
2017-08-10 20:44:22 +0100

    First ACTUAL commit

---
+++ tutorial.c
@@ -0,0 +1,20 @@
+/*#include <string.h>
+#include <stdlib.h>
+*/
+
+#include <unistd.h>
+#include <stdio.h>
+
+int main()
+{
+	printf("Hello World!");
+	sleep (10);
+}
+
+    /*
+    int age;
+    printf("Hello, please enter your age:\n");
+    scanf("%d", &age);
+    printf("Your age is %d", age);
+    return 0;
+    */
